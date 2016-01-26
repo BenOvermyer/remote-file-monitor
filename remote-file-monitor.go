89ed779941b08282714e285f179d8f3730de6d1d
Ben Overmyer <manatrance@gmail.com>
2016-01-26 14:25:58 -0600

    Added HTTP status codes for some error states.

--- remote-file-monitor.go
+++ remote-file-monitor.go
@@ -13,21 +13,30 @@ func main() {
 
 func sizeHandler(w http.ResponseWriter, r *http.Request) {
   var url = r.FormValue("url")
+
+  if len(url) == 0 {
+    w.WriteHeader(http.StatusBadRequest)
+    return
+  }
+
   resp, err := http.Get(url)
 
   if err != nil {
-    fmt.Fprintf(w, "%s", err)
+    w.WriteHeader(http.StatusInternalServerError)
+    return
   } else {
     defer resp.Body.Close()
 
     body, err := ioutil.ReadAll(resp.Body)
 
     if err != nil {
-      fmt.Fprintf(w, "%s", err)
+      w.WriteHeader(http.StatusInternalServerError)
+      return
     }
 
     var contents = string(body)
 
     fmt.Fprintf(w, "%d", len(contents))
+    return
   }
 }
